0a1d51f814f91e1f6c724547bec450140be5a370
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2024-09-26 12:01:20 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const money = 400;
+if( money > 500){
+    console.log('you get the cook for free');
+    
+}
+else{
+    if(money < 500){
+        console.log(' you pay the 30 taka for cook');
+        
+    }
+}
