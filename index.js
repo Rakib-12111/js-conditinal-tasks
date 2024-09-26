25a38b732a06732a7e9d285cae175c5c72a0e75f
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2024-09-26 13:08:14 +0600

    new file added

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
-const money = 400;
-if( money > 500){
-    console.log('you get the cook for free');
+// const money = 400;
+// if( money > 500){
+//     console.log('you get the cook for free');
+    
+// }
+// else{
+//     if(money < 500){
+//         console.log(' you pay the 30 taka for cook');
+        
+//     }
+// }
+
+
+// const money = 600;
+// if(money > 500){
+//     console.log('you get the for free drink');
+    
+// }
+// else{
+//     console.log('I have pay the : 30taka');
+    
+// }
+
+
+
+
+
+
+
+var examResult = 60;
+if( examResult > 90){
+    console.log('A+');
     
 }
 else{
-    if(money < 500){
-        console.log(' you pay the 30 taka for cook');
+    if(examResult > 80){
+        console.log('A');
         
     }
+    else{
+        if(examResult > 70){
+            console.log('B+');
+            
+        }
+        else{
+            if(examResult > 60){
+                console.log("C");
+                
+            }
+            else{
+                if( examResult > 0){
+                 console.log('Sadron pass');
+                 
+                }
+            }
+
+        }
+       
+        
+    }
+    
 }
---
+++ nested.js
@@ -0,0 +1,80 @@
+// var socore = 20;
+// if(socore > 80){
+//     console.log('then go for a lunc');
+    
+// }
+// else{
+//     if(socore >= 60){
+//         console.log('good luck next time');
+        
+//     }
+//     else{
+//         if( socore > 40){
+//             console.log('keep your fried message unseen');
+            
+
+//         }
+//         else{
+//             console.log('tui mar bondu na');
+            
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+// var garivara = 500;
+// var age = 12;
+// var isStudent = true;
+// if( age <= 10){
+//     console.log('you can for free ');
+    
+// }
+// if(isStudent ===true){
+//    // 50% discount
+//    discount = garivara * 50 / 100;
+//    payAmount = garivara - discount;
+//    console.log(payAmount);
+   
+   
+// }
+// if( age >= 50){
+//     // 15 % discount 
+//     discount = garivara * 15 / 100;
+//     payAmount = garivara - discount;
+//     console.log(payAmount);
+    
+// }
+// else{
+//     console.log(garivara);
+    
+// }
+
+
+
+
+
+
+// chaile index diya man paite pari tarpor man set korte pari and man change korte pari
+
+// const numbers = [ 88, 77, 99, 421, 54, 63]
+// console.log(numbers);
+// console.log(numbers[2]); // j kono man nite pari array thaki
+// const fourth = numbers[3]
+// console.log(fourth);
+
+// numbers[1] = 99999;
+// console.log(numbers);
+
+
+
+
+
+
