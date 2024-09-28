d91574f5dec6fb321f80cb6a76862a5aaeb3d4f3
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2024-09-28 08:27:52 +0600

    function added

--- for.sum.js
+++ for.sum.js
@@ -21,7 +21,352 @@
 
 
 // deceremnt meltent for loop]
-for(let i = 20; i >= 0; i-- ){
-    console.log(i);
+// for(let i = 20; i >= 0; i-- ){
+//     console.log(i);
     
+// }
+
+// let n = 20;
+// while( n >= 0){
+//     console.log(n);
+//     n --;
+    
+// }
+
+
+// let n = 100;
+// while( n >= 5){
+//     console.log(n);
+//     n --;
+    
+
+
+
+// let n = 20;
+// let sum  = 0;
+// while( n <= 100){
+//     console.log(n);
+//     sum = sum + n;
+//     n++;
+    
+// }
+
+
+
+// console.log('mot number', sum);
+
+
+
+
+// let total = 0;
+// for(let i = 1; i<= 20; i ++){
+//     if(i %3 === 0){
+//         console.log(i);
+//         total = total + i;
+        
+//     }
+    
+// }
+
+
+
+
+
+
+// console.log(total);
+
+
+
+
+
+
+
+
+
+// for( let i = 0; i < 20; i ++){
+//     console.log(i);
+//     if( i >= 12){
+//         break;
+//     }
+// }
+
+
+// let i = 50;
+// while( i >= 1){
+//     console.log(i);
+//     if( i < 20){
+//         break;
+//     }
+//     i --;
+    
+// }
+// let i = 50;
+// while( i >= 1){
+//     console.log(i);
+//     if( i <= 20){
+//         break;
+//     }
+//     i --;
+// }
+
+
+// let i =1;
+// while( i <= 20){
+//     console.log(i);
+//     if( i <= 3){
+//         break;
+//     }
+//     i ++;
+// }
+
+
+
+
+// let i = 20;
+// while( i <= 100){
+//     console.log(i);
+//     if( i <= 10){
+//         break;
+//     }
+//     i ++;
+// }
+
+
+
+
+
+
+
+
+
+// let i = 10;
+// while( i <= 50){
+//     console.log(i);
+//     if( i >= 30){
+//         break;
+//     }
+//     i ++;
+    
+// }
+
+
+// let i = 30;
+// while( i >= 0){
+//     console.log(i);
+//     if( i <= 20){
+//         break
+//     }
+//     i --;
+// }
+
+
+
+
+
+
+
+
+
+// const drink = ' water  '
+// const liquid = 'water'
+// if( drink.trim() === liquid.trim()){
+//     console.log('pani er opor nam life');
+    
+// }
+// else{
+//     console.log('somudre pani ase khaite pari na');
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const sentence = ' I am learing web dev'
+// let reverse = ''
+// for( const letter of sentence){
+//     reverse = letter + reverse;
+   
+    
+// }
+
+
+
+
+// console.log(reverse);
+
+
+
+
+// const sentence = ' I am learing web dev'
+// let rev = ''
+// for(let i = 0; i < sentence.length; i ++){
+//    rev = le
+// }
+
+
+
+
+
+
+
+
+// const school = 'RAJ Uk uttara Model Schllo'
+// console.log(school);
+// console.log(school.toLowerCase());
+
+
+
+
+
+
+
+
+
+
+// const drink = ' water'
+// const liquid = 'water'
+// if(drink.trim() === liquid.trim()){
+//    console.log('panir opar nam jubon');
+
+   
+// }
+// else{
+//    console.log('ami pani khaite pari na');
+   
+// }
+
+
+
+
+
+
+
+// const address = 'andorkilla';
+// const part = address.slice( 2, 4)
+// console.log(part);
+
+
+
+
+
+
+// const sentence = ' I am a good and hardworking person'
+// console.log(sentence.split(' a '));
+
+
+
+
+// const friendStr = 'Rahim, kahim, adhim lahim, fahim'
+// console.log(friendStr.split( ','));
+
+
+
+
+
+// const realFrined = [ 'Rahim', ' kahim', ' adhim lahim', ' fahim' ];
+// console.log(realFrined);
+// console.log(realFrined.join( ','));
+
+
+
+
+
+
+
+// const first = 'M Rakib'
+// const lalst = 'Ahmed'
+// const fullName2 = first.concat( ' ', lalst)
+// console.log(fullName2);
+
+// // const fullName = first + lalst;
+// // console.log(fullName);
+
+
+
+
+
+
+
+
+
+
+// const world = ' hell world hello world'
+// console.log(world.includes("q"));
+
+
+
+
+
+// const fullName = " Hello Friend My name is M Rakib Ahmed Raiyan "
+// console.log(fullName.includes('tmi amr jibon ho'));
+
+
+
+
+
+
+// const sentence = 'i am learning web deb';
+// let reverse = ''
+// for( const letter of sentence){
+//    console.log(letter);
+//    reverse = letter + reverse
+   
+// }
+// console.log(reverse);
+
+
+
+
+// var fullName = ' My Name is M Rakib Ahmed Rakib'
+// var revese = fullName.split('').reverse().join('')
+// console.log(revese);
+
+
+
+
+
+
+
+
+// const sentence = ' I am rearing web Dev'
+// const reverse = sentence.split('').reverse().join('');
+// console.log(reverse);
+
+
+
+
+
+
+const sentence = ' I am rearing web Dev'
+let reverse = '';
+for( const letter of sentence){
+ // console.log(letter);
+  reverse = letter + reverse;
+   
+   
 }
+
+
+console.log(reverse);
+
+
+
+
---
+++ function.js
@@ -0,0 +1,93 @@
+// function doubleIt(number){
+//     const doubleIt = number * 3;
+//     console.log(number,doubleIt);
+    
+// }
+
+
+// console.log('I will call the function');
+// doubleIt(15)
+// doubleIt(10)
+
+
+
+
+
+
+// function differecrnt(num1, num2){
+//     const tolat = num1 + num2;
+// }
+
+// console.log(num1,num2,'difference',tolat);
+// differecrnt( 10, 10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function differecent ( num1, num2){
+//     const difference = num1 - num2;
+//     //console.log(num1,num2,"difference",difference);
+    
+//     console.log(difference);
+// }
+
+// const fatherAge = 50;
+// const Myage = 20;
+// differecent( fatherAge, Myage)
+
+
+
+
+
+
+
+
+
+
+
+// function calculateSaving(income){
+//     if(typeof income !== 'number'){
+//         return 'invalid number'
+//     }
+//     const houseRent = 5000;
+//     const mealCosnt = 3000;
+//     const personalExpnese =2000;
+//     const tolatExpenses = houseRent + mealCosnt + personalExpnese;
+//     if( income < tolatExpenses){
+//      return ' Earn more vai khoros besi kora fealcho'
+//     }
+//     if( income > tolatExpenses){
+//         return ' Donate succuss full bro tmi anock valo manush'
+//     }
+//     const  savings = income - tolatExpenses;
+//     return savings;
+
+// }
+
+
+// const result  = calculateSaving(2000000);
+
+// console.log(result);
+
+
+
+
+
+
+
+
+
